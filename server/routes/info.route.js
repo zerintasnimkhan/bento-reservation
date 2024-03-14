baa90ff89163d1b91a99240b9d904a3cedd129f5
Zerin Tasnim <zerintasnimriana@gmail.com>
2024-03-14 13:16:40 +0600

    util: removed extra package.json file

--- server/routes/info.route.js
+++ server/routes/info.route.js
@@ -7,15 +7,8 @@ router.post("/add", attachUser, infoController.createReservation);
 router.post("/availableRestaurants", infoController.allAvailableRestaurants);
 router.get("/getRestaurant/:restaurantId", infoController.fetchRestaurantById);
 router.get("/allRestaurants", infoController.getAllRestaurants);
-router.get("/allReservations/:restaurantId",
-  infoController.getReservationsByRestaurantId
-);
-router.get(
-  "/allReservations/restaurant/:restaurantId/date/:date",
-  infoController.getReservationsByRestaurantIdAndDate
-);
-router.put(
-  "/change-status/reservation/:reservationId/status/:status",
-  infoController.changeReservationStatus
-);
+router.get("/allReservations/:restaurantId", infoController.getReservationsByRestaurantId);
+router.get("/allReservations/restaurant/:restaurantId/date/:date", infoController.getReservationsByRestaurantIdAndDate);
+router.put("/change-status/reservation/:reservationId/status/:status", infoController.changeReservationStatus);
+
 module.exports = router;
